25882e93cf7c49f73f1a91ac4325e5c1e81b37c0
Rishabh Verma <rishabhv@live.com>
2023-08-24 17:38:31 +0530

    chore: clean up and test CI.

--- Chapter 7/ConnectedService/Extensions.cs
+++ Chapter 7/ConnectedService/Extensions.cs
@@ -5,8 +5,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-// September 2023
-// This project is just for illustration purposes and is not complete
 
 using EnvDTE;
 using Microsoft.VisualStudio;
